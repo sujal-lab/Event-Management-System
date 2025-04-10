999f5b979d4190b3bf277c5d5fb28e6504e35c98
sujal-lab <sujalkandari11@gmail.com>
2025-04-10 21:40:55 +0530

    Updated user schema

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: 'attendee' }
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'organizer'], // Allowed roles
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now // Automatically set creation date
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
